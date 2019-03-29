404a699341322199139cbc898a82f58ec772dcb6
李樟取 <lizhangqu@weidian.com>
2019-03-29 17:53:41 +0800

    * demo

--- app/src/main/java/io/github/lizhangqu/app/MainActivity.java
+++ app/src/main/java/io/github/lizhangqu/app/MainActivity.java
@@ -3,6 +3,11 @@ package io.github.lizhangqu.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.BasicHttpEntity;
+
+import java.io.IOException;
+
 /**
  * @author lizhangqu
  * @version V1.0
@@ -10,10 +15,19 @@ import android.os.Bundle;
  */
 public class MainActivity extends AppCompatActivity {
 
+    HttpEntity httpEntity=new BasicHttpEntity();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HttpEntity httpEntity = new BasicHttpEntity();
+        try {
+            httpEntity.getContent();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
